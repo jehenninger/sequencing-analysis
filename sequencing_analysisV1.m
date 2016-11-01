f46045875057e8b4a246d959db5ab673f8ae10f6
jehenninger <j.e.henninger+git@gmail.com>
2016-11-01 16:31:39 -0400

    read fastq and parse

--- sequencing_analysisV1.m
+++ sequencing_analysisV1.m
@@ -1,8 +1,27 @@
-test = 'Hello world';
-test2 = 'test';
+[fileName, pathName] = uigetfile('E:\Zon Lab\Sequencing\MGH SEQUENCING\*.fastq');
 
-disp(test);
-disp(test2);
 
-%Surface test
-x = 10;
+% Read in fastq file
+fastq_seq = fastqread(fullfile(pathName,fileName));
+
+%% Separate paired end sequencing into 2 files
+
+% Vectorized
+idx = ~cellfun('isempty',strfind({fastq_seq.Header},'1:N'));
+pairedEndTest1 = fastq_seq(idx);
+pairedEndTest2 = fastq_seq(~idx);
+
+if ~exist(fullfile(pathName,'Parsed Output'),'dir')
+    mkdir(pathName,'Parsed Output');
+end
+
+if ~exist(fullfile(pathName,'Parsed Output','Paired1'),'dir')
+    mkdir(fullfile(pathName,'Parsed Output'),'Paired1');
+end
+
+if ~exist(fullfile(pathName,'Parsed Output','Paired2'),'dir')
+    mkdir(fullfile(pathName,'Parsed Output'),'Paired2');
+end
+
+fastqwrite(fullfile(pathName,'Parsed Output','Paired1',[fileName,'(1).fastq']),pairedEndTest1);
+fastqwrite(fullfile(pathName,'Parsed Output','Paired2',[fileName,'(2).fastq']),pairedEndTest2);
