83035ef8ef5e8276fc32d3ab18fe465ea2bbca9a
jehenninger <j.e.henninger+git@gmail.com>
2016-11-09 19:49:37 -0500

    fixed errors

--- trimReads.m
+++ trimReads.m
@@ -11,19 +11,19 @@ trimIdx = trimIdx(trimLongReadsIdx);
 qual = qual(trimLongReadsIdx);
 
 headerNew = header(trimLongReadsIdx);
-steps = length(trimIdx);
-hWait = waitbar(0,'Trimming reads...');
-seqNew = cell(steps,1);
-qualNew = cell(steps,1);
-for kk = 1:steps
-    seqNew{kk} = seq{kk}(trimIdx{kk});
-    qualNew{kk} = qual{kk}(trimIdx{kk});
-    waitbar(kk/steps);
-end
-close(hWait);
+% steps = length(trimIdx);
+% hWait = waitbar(0,'Trimming reads...');
+% seqNew = cell(steps,1);
+% qualNew = cell(steps,1);
+% for kk = 1:steps
+%     seqNew{kk} = seq{kk}(trimIdx{kk});
+%     qualNew{kk} = qual{kk}(trimIdx{kk});
+%     waitbar(kk/steps);
+% end
+% close(hWait);
 
-% seqNew = cellfun(@(x,y) x(y), seq, trimIdx,'UniformOutput',false);
-% qualNew = cellfun(@(x,y) x(y), qual, trimIdx, 'UniformOutput',false);
+seqNew = cellfun(@(x,y) x(y), seq, trimIdx,'UniformOutput',false);
+qualNew = cellfun(@(x,y) x(y), qual, trimIdx, 'UniformOutput',false);
 
 fastq_seq_new = struct('Header',headerNew,'Sequence',seqNew,'Quality',qualNew);
 
